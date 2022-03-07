887f8f1cb2173916f789047d688dd62a9c146480
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-03-07 10:23:23 +0530

    Update CheckBatteryChargingCurrentRange.c

--- CheckBatteryChargingCurrentRange.c
+++ CheckBatteryChargingCurrentRange.c
@@ -3,7 +3,7 @@
 
 int getLowerLimitCurrent( int CurrentSamples[] , int no_of_samples ){
   int lowerLimit = CurrentSamples[0];
-  for (i = 1 , i < no_of_samples , i++ ){
+  for (i = 1 ; i < no_of_samples ; i++ ){
     if(lowerLimit > CurrentSamples[i]){
       lowerLimit = CurrentSamples[i];
     }
@@ -14,7 +14,7 @@ int getLowerLimitCurrent( int CurrentSamples[] , int no_of_samples ){
 
 int getUpperLimitCurrent( int CurrentSamples[] , int no_of_samples ){
   int upperLimit = CurrentSamples[0];
-  for (i = 1 , i < no_of_samples , i++ ){
+  for (i = 1 ; i < no_of_samples ; i++ ){
     if(upperLimit < CurrentSamples[i]){
       upperLimit = CurrentSamples[i];
     }
