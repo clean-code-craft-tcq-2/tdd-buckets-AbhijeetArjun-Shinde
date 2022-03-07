b921aa7042dae98973ba15134b77273e8abac324
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-03-07 09:00:50 +0530

    Create CheckCurrentRange.cpp

---
+++ CheckCurrentRange.cpp
@@ -0,0 +1,7 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+#include "typewise-alert.h"
+
+TEST_CASE("Infers the battery charging current range and sample occurences in particular range") {
+}
