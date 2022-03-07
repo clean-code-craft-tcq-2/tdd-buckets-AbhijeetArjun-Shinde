4461b9b79b43b59989c1ef0ad8c1717f3bccbf5f
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-03-07 09:15:52 +0530

    Update CheckCurrentRange.cpp

--- CheckCurrentRange.cpp
+++ CheckCurrentRange.cpp
@@ -1,7 +1,14 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
-#include "typewise-alert.h"
+#include <stdbool.h>
 
 TEST_CASE("Infers the battery charging current range and sample occurences in particular range") {
+  bool result;
+  char ComputedRange[];
+  int CurrentSamples[] = {8,2,6,3,4,5,8,2,3,7,4,3,3};
+  int NoOfSamples = sizeof(CurrentSamples)/sizeof(CurrentSamples[0]);
+  ComputedRange = CalculateChargingCurrentRange(CurrentSamples,NoOfSamples);
+  result = strcmp(ComputedRange, "2-8 , 13");
+  REQUIRE(result == 0);
 }
