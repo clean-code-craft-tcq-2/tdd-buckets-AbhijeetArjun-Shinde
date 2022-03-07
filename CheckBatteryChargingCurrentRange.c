b4f1e3c0198306dfb5b2c50e0ce35119f9f2883d
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-03-07 10:17:37 +0530

    Update CheckBatteryChargingCurrentRange.c

--- CheckBatteryChargingCurrentRange.c
+++ CheckBatteryChargingCurrentRange.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include "CheckBatteryChargingCurrentRange.h"
-char* CalculateChargingCurrentRange(int currentsamples[] , int no_of_samples){
+
+int getLowerLimitCurrent( int CurrentSamples[] , int no_of_samples ){
+  int lowerLimit = CurrentSamples[0];
+  for (i = 1 , i < no_of_samples , i++ ){
+    if(lowerLimit > CurrentSamples[i]){
+      lowerLimit = CurrentSamples[i]
+    }
+  } 
   
-  return "  ";
+  return lowerLimit;
+}
+
+int getUpperLimitCurrent( int CurrentSamples[] , int no_of_samples ){
+  int upperLimit = CurrentSamples[0];
+  for (i = 1 , i < no_of_samples , i++ ){
+    if(upperLimit < CurrentSamples[i]){
+      upperLimit = CurrentSamples[i]
+    }
+  } 
+  return upperLimit;
+}
+
+char* CalculateChargingCurrentRange(int CurrentSamples[] , int no_of_samples){
+  int LowerLimitCurrent = 0 ;
+  int UpperLimitCurrent = 0 ;
+  LowerLimitCurrent = getLowerLimitCurrent(CurrentSamples , no_of_samples);
+  UpperLimitCurrent = getUpperLimitCurrent(CurrentSamples , no_of_samples);
+  sprintf(buffer , "%d - %d, %d", LowerLimitCurrent , UpperLimitCurrent , no_of_samples);
+  return buffer;
   
 }
