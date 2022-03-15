9d2c11993e69cf30e77f5b1447bb16f9dd3ef63d
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-03-15 10:58:26 +0530

    Update CheckBatteryChargingCurrentRange.c

--- CheckBatteryChargingCurrentRange.c
+++ CheckBatteryChargingCurrentRange.c
@@ -15,20 +15,45 @@ int* SortCurrentSamples( int CurrentSamples[] , int no_of_samples ){
   return CurrentSamples;
 }
 
-int getUpperLimitCurrent( int CurrentSamples[] , int no_of_samples ){
-  int upperLimit = CurrentSamples[0];
-  int i;
-  for (i = 1 ; i < no_of_samples ; i++ ){
-    if(upperLimit < CurrentSamples[i]){
-      upperLimit = CurrentSamples[i];
+char* getCurrentLimits( int CurrentSamples[] , int no_of_samples ){
+  int *SortedSamples;
+  SortedSamples = SortCurrentSamples(CurrentSamples,no_of_samples);
+  
+  int upperLimit[no_of_samples/2] = 0;
+  int lowerLimit[no_of_samples/2] = 0;
+  int NoOfSamples[no_of_samples/2] = 0;
+  
+  int j = 0 ;
+  lowerLimit[j] = *(SortedSamples);
+  upperLimit[j] = *(SortedSamples + no_of_samples -1);
+  NoOfSamples[j] = no_of_samples;
+  
+  for (int i = 0 ; i < no_of_samples ; i++ ){
+    if((*(SortedSamples+i+1) - *(SortedSamples+i)) >=2){
+      upperLimit[j] = *(SortedSamples+i);
+      lowerLimit[j+1] = *(SortedSamples+i+1);
+      NoOfSamples[j] = i+1;
+      
+      upperLimit[j+1] = *(SortedSamples+no_of_samples -1);
+      NoOfSamples[j+1] = no_of_samples - (i+1);
+      j++;
     }
   } 
-  return upperLimit;
+
+  char outputString[no_of_samples/2][50];
+  char *buffer = outputString[0];
+  sprintf(buffer , "%d - %d, %d", lowerLimit[0] , upperLimit[0] , NoOfSamples[0]);
+  
+  for(int k=1; k < no_of_samples/2 ; k++){
+    if(lowerLimit[k] !=0 ){
+      char *buffer = outputString[k];
+      sprintf(buffer , "%d - %d, %d", lowerLimit[k] , upperLimit[k] , NoOfSamples[k]);
+    }
+  }
+ return outputString ;
 }
 
 char* CalculateChargingCurrentRange(int CurrentSamples[] , int no_of_samples){
-  int *SortedSamples;
-  int UpperLimitCurrent = 0 ;
   char outputString[50];
   char *buffer = outputString;
   SortedSamples = SortCurrentSamples(CurrentSamples , no_of_samples);
