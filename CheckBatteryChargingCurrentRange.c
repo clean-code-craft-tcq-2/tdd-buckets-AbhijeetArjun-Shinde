af1c37b3cb9223c7795978c34569d715ed30f154
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-03-16 08:57:51 +0530

    Update CheckBatteryChargingCurrentRange.c

--- CheckBatteryChargingCurrentRange.c
+++ CheckBatteryChargingCurrentRange.c
@@ -29,9 +29,9 @@ char* getCurrentLimits( int *CurrentSamples , int no_of_samples ){
   int lowerLimit[no_of_samples/2] = {0};
   int NoOfSamples[no_of_samples/2] = {0};
   
-  lowerLimit[j] = *(CurrentSamples);
-  upperLimit[j] = *(CurrentSamples + no_of_samples -1);
-  NoOfSamples[j] = no_of_samples;
+  lowerLimit[0] = *(CurrentSamples);
+  upperLimit[0] = *(CurrentSamples + no_of_samples -1);
+  NoOfSamples[0] = no_of_samples;
   
   char *OutputString;
   OutputString = OutputFormatter(lowerLimit,upperLimit,NoOfSamples,no_of_samples);
