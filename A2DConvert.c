db9f22681b311e5bbb45d4ec54256f12b52edab2
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-03-28 12:04:09 +0530

    Update A2DConvert.c

--- A2DConvert.c
+++ A2DConvert.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include "A2DConvert.h"
 
+int ConvertToAmp(int CurrentSample){
+  return (int)((10 * CurrentSample)/4094);
+}
+
+
 void CheckConvertSensedCurrentToAmp(int *CurrentSamples, int NoOfSamples, int *ConvertedSamples){
+  
+  for(int i = 0; i < NoOfSamples;i++){
+    if(*(CurrentSamples+i) <= 4094){
+      *(ConvertedSamples+i) = ConvertToAmp(*(CurrentSamples+i));
+    }
+    else{
+     printf("Invalid Input\n");
+     *(ConvertedSamples+i) = 0;       
+    }
+  }
 }
