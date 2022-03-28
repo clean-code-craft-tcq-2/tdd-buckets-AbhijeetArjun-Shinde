a4515dd43b352d61d03f9df3ce54e08ad75b87ea
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-03-28 15:09:05 +0530

    Update CheckCurrentRange.cpp

--- CheckCurrentRange.cpp
+++ CheckCurrentRange.cpp
@@ -17,7 +17,7 @@
 }*/
 
 
-TEST_CASE("Test for Checking sensed current values within range") {
+TEST_CASE("Test for Checking sensed current values within range- Sensor 1 ") {
   int SensedCurrentSamples1[] =  {0,1000,1250,1750,2300,4000,4094,3200,3500,5000,4095,3094};
   int NoOfSamples1 = sizeof(SensedCurrentSamples1)/sizeof(SensedCurrentSamples1[0]);
   bool result=0;
@@ -41,5 +41,22 @@ TEST_CASE("Test for Checking sensed current values within range and for Checking
   char *ComputedRange;
   ComputedRange = CalculateChargingCurrentRange(SensedCurrentSamples,NoOfSamples);
   result = strcmp(ComputedRange, expectedOutput);
+  REQUIRE(result == 0); 
+}
+
+
+TEST_CASE("Test for Checking sensed current values within range- Sensor 2 ") {
+  int SensedCurrentSamples2[] =  {0,100,200,330,423,449,499,510,511,512,625,679,800,917,950,1022,1024,1500};
+  int NoOfSamples2 = sizeof(SensedCurrentSamples2)/sizeof(SensedCurrentSamples2[0]);
+  bool result=0;
+  int ConvertedOutput1[NoOfSamples2];
+  int *ConvertedOutputPtr1=ConvertedOutput1 ;
+  CheckConvertSensedCurrentToAmp1(SensedCurrentSamples2,NoOfSamples2,ConvertedOutputPtr1);
+  int expectedOutput2[] = {15,12,9,5,3,2,0,0,0,0,3,5,8,12,13,15,0,0}; 
+  for(int i =0; i < NoOfSamples2;i++){
+    printf("%d\n", *(ConvertedOutputPtr1+i));
+    if(*(ConvertedOutputPtr1+i) != expectedOutput2[i])
+      result=1;
+  }
   REQUIRE(result == 0); 
 }
