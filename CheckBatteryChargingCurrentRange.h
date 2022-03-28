b468f54d3fdde64b9358b3852badd4832b535981
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-03-28 09:43:54 +0530

    Update CheckBatteryChargingCurrentRange.h

--- CheckBatteryChargingCurrentRange.h
+++ CheckBatteryChargingCurrentRange.h
@@ -3,4 +3,4 @@
 char* CalculateChargingCurrentRange(int currentsamples[] , int no_of_samples);
 int* SortCurrentSamples( int CurrentSamples[] , int no_of_samples );
 char* getCurrentLimits( int CurrentSamples[] , int no_of_samples );
-char* OutputFormatter(int* lowerLimit,int* upperLimit, int* NoOfSamples,int no_of_samples);
+char* OutputFormatter(int* lowerLimit,int* upperLimit, int* NoOfSamples,int no_of_samples,char *buffer1);
