b36b3138b49bddf74f30ed86281624c160d09338
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-03-28 10:07:52 +0530

    Update CheckCurrentRange.cpp

--- CheckCurrentRange.cpp
+++ CheckCurrentRange.cpp
@@ -15,3 +15,15 @@ TEST_CASE("Test for Checking Upper and Lower Limits of Current Samples") {
   result = strcmp(ComputedRange, expectedOutput);
   REQUIRE(result == 0); 
 }
+
+
+TEST_CASE("Test for Checking Upper and Lower Limits of Current Samples") {
+  int CurrentSamples[] =  {5,2,10,3,13,5,8,2,14,7,15,3,17,18,19,20};
+  int NoOfSamples = sizeof(CurrentSamples)/sizeof(CurrentSamples[0]);
+  bool result;
+  char *ComputedRange;
+  ComputedRange = CalculateChargingCurrentRange(CurrentSamples,NoOfSamples);
+  char expectedOutput[] = "2 - 3, 4\n 5 - 5,2\n8 - 8,1\n10 - 10,1\n13 - 15,3\n17 - 20,4"; 
+  result = strcmp(ComputedRange, expectedOutput);
+  REQUIRE(result == 0); 
+}
