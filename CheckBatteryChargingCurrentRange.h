ef355c32792bcf9dbc477e2da06890d2e1867988
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-03-28 11:57:43 +0530

    Update CheckBatteryChargingCurrentRange.h

--- CheckBatteryChargingCurrentRange.h
+++ CheckBatteryChargingCurrentRange.h
@@ -1,5 +1,5 @@
 #define MAX_LIMIT_SEPRATION_BW_INTERVALS 2
 
 char* CalculateChargingCurrentRange(int currentsamples[] , int no_of_samples);
-char* getCurrentLimits( int *CurrentSamples , int no_of_samples,char *OutputString );
-char* OutputFormatter(int* lowerLimit,int* upperLimit, int* NoOfSamples,int no_of_samples,char *buffer1);
+void getCurrentLimits( int *CurrentSamples , int no_of_samples,char *OutputString );
+void OutputFormatter(int* lowerLimit,int* upperLimit, int* NoOfSamples,int no_of_samples,char *buffer1);
