af1fbe317d82637a89509a3fe31d686894ba050b
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-03-28 09:43:29 +0530

    Update CheckBatteryChargingCurrentRange.c

--- CheckBatteryChargingCurrentRange.c
+++ CheckBatteryChargingCurrentRange.c
@@ -8,24 +8,8 @@ int CompareFunc (const void * a, const void * b) {
    return ( *(int*)a - *(int*)b );
 }
 
-int* SortCurrentSamples( int CurrentSamples[] , int no_of_samples ){
-  int i, j, temp;
-  for (i = 0; i < (no_of_samples - 1); i++){
-   for (j = 0; j < no_of_samples - 1 - i; j++ ){
-    if (CurrentSamples[j] > CurrentSamples[j+1]){
-        temp = CurrentSamples[j+1];
-        CurrentSamples[j+1] = CurrentSamples[j];
-        CurrentSamples[j] = temp;
-        }
-      }
-    }  
-  return CurrentSamples;
-}
-
-char* OutputFormatter(int* lowerLimit,int* upperLimit, int* NoOfSamples,int no_of_samples){
-  char outputString1[50];
+char* OutputFormatter(int* lowerLimit,int* upperLimit, int* NoOfSamples,int no_of_samples,char *buffer1){
   char outputString2[50];
-  char *buffer1 = outputString1;
   char *buffer2 = outputString2;
   sprintf(buffer1 , "%d - %d, %d", lowerLimit[0] , upperLimit[0] , NoOfSamples[0]);
   
@@ -40,13 +24,13 @@ char* OutputFormatter(int* lowerLimit,int* upperLimit, int* NoOfSamples,int no_o
  return buffer1 ;
 }
 
-char* getCurrentLimits( int *CurrentSamples , int no_of_samples ){
+char* getCurrentLimits( int *CurrentSamples , int no_of_samples){
   int upperLimit[no_of_samples/2] = {0};
   int lowerLimit[no_of_samples/2] = {0};
   int NoOfSamples[no_of_samples/2] = {0};
   int j = 0, cntr=0 ;
   
-  //AT beginning lower limit = first element of sorted array, upper limit= last element of sorted array, no of samples in this range = samples in given inpuy array
+  //AT beginning lower limit = first element of sorted array, upper limit= last element of sorted array, no of samples in this range = samples in given input array
   lowerLimit[j] = *(CurrentSamples);
   upperLimit[j] = *(CurrentSamples + no_of_samples -1);
   NoOfSamples[j] = no_of_samples;
@@ -57,25 +41,22 @@ char* getCurrentLimits( int *CurrentSamples , int no_of_samples ){
     if((*(CurrentSamples+i+1) - *(CurrentSamples+i)) >=MAX_LIMIT_SEPRATION_BW_INTERVALS){
       upperLimit[j] = *(CurrentSamples+i);
       
-      
       lowerLimit[j+1] = *(CurrentSamples+i+1);
       upperLimit[j+1] = *(CurrentSamples+no_of_samples -1);
       j++;
       cntr = 0;
     }
- 
   } 
-  char *OutputString;
-  OutputString = OutputFormatter(lowerLimit,upperLimit,NoOfSamples,no_of_samples);
+   
+  char outputStr[50];
+  char *OutputString = outputStr;
+  OutputString = OutputFormatter(lowerLimit,upperLimit,NoOfSamples,no_of_samples,OutputString);
   return OutputString;
-  
 }
 
 char* CalculateChargingCurrentRange(int currentsamples[] , int no_of_samples){
   qsort(currentsamples, no_of_samples, sizeof(int), CompareFunc);
-  for (int h =0; h < no_of_samples ; h++){
-     printf("%d \n ", currentsamples[h]);
-  }
+   
   char *OutputString;
   OutputString = getCurrentLimits(currentsamples ,no_of_samples);
   return OutputString;
