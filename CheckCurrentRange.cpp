100208b2412a177c196230c4b65e39ad560d3063
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-03-28 09:46:35 +0530

    Update CheckCurrentRange.cpp

--- CheckCurrentRange.cpp
+++ CheckCurrentRange.cpp
@@ -4,16 +4,6 @@
 #include <stdbool.h>
 #include "CheckBatteryChargingCurrentRange.h"
 
-TEST_CASE("Test for Sorting Current Samples") {
-  int CurrentSamples[] = {5,2,6,3,4,5,8,2,3,7,4,3,3};
-  int ExpectedOutput[] = {2,2,3,3,3,3,4,4,5,5,6,7,8};
-  int NoOfSamples = sizeof(CurrentSamples)/sizeof(CurrentSamples[0]);
-  int *SortedSamples;
-  SortedSamples = SortCurrentSamples(CurrentSamples,NoOfSamples);
-  for(int k=0; k < NoOfSamples ; k++){
-    REQUIRE( *(SortedSamples+k) == ExpectedOutput[k]);
-  }  
-}
 
 TEST_CASE("Test for Checking Upper and Lower Limits of Current Samples") {
   int CurrentSamples[] =  {5,2,6,3,4,5,8,2,3,7,4,3,3};
