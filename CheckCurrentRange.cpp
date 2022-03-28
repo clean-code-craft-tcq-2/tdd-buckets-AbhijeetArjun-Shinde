be0b7cfb983e57e8648091a92cfa6c2f33bba6b1
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-03-28 13:43:15 +0530

    Update CheckCurrentRange.cpp

--- CheckCurrentRange.cpp
+++ CheckCurrentRange.cpp
@@ -5,7 +5,7 @@
 #include "CheckBatteryChargingCurrentRange.h"
 #include "A2DConvert.h"
 
-TEST_CASE("Test for Checking Upper and Lower Limits of Current Samples") {
+/*TEST_CASE("Test for Checking Upper and Lower Limits of Current Samples") {
   int CurrentSamples[] =  {2,3,4,5,2,3,4,7,8,9,8,7,5,6,7,12,14,15,11,12};
   int NoOfSamples = sizeof(CurrentSamples)/sizeof(CurrentSamples[0]);
   bool result;
@@ -31,7 +31,7 @@ TEST_CASE("Test for Checking sensed current values within range") {
       result=1;
   }
   REQUIRE(result == 0); 
-}
+}*/
 
 TEST_CASE("Test for Checking sensed current values within range and for Checking Upper and Lower Limits of Current Samples") {
   int SensedCurrentSamples[] =  {0,1000,1250,1750,2300,4000,4094,3200,3500,5000,4095,3094};
