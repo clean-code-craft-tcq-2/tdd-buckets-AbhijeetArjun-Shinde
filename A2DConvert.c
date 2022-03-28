8e2791a65d9909e7d3f74bcbbc47d3701bd817a3
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-03-28 13:23:40 +0530

    Update A2DConvert.c

--- A2DConvert.c
+++ A2DConvert.c
@@ -23,7 +23,7 @@ int ConvertToAmp(int CurrentSample){
 void CheckConvertSensedCurrentToAmp(int *CurrentSamples, int NoOfSamples, int *ConvertedSamples){
   
   for(int i = 0; i < NoOfSamples;i++){
-    if(*(CurrentSamples+i) <= 4094){
+    if(*(CurrentSamples+i) <= 4094 && *(CurrentSamples+i) >= 0){
       *(ConvertedSamples+i) = ConvertToAmp(*(CurrentSamples+i));
     }
     else{
