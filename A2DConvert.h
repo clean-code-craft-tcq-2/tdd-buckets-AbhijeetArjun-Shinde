96602be77e771d2fdebbdbece29955ba4e15f23f
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-03-29 08:11:54 +0530

    Update A2DConvert.h

--- A2DConvert.h
+++ A2DConvert.h
@@ -1,2 +1,5 @@
-void CheckConvertSensedCurrentToAmp(int *CurrentSamples, int NoOfSamples, int *ConvertedSamples);
-void CheckConvertSensedCurrentToAmp1(int *CurrentSamples, int NoOfSamples, int *ConvertedSamples);
+void CheckAndConvertSensedCurrentToAmp_Sensor1(int *CurrentSamples, int NoOfSamples, int *ConvertedSamples);
+void CheckAndConvertSensedCurrentToAmp_Sensor2(int *CurrentSamples, int NoOfSamples, int *ConvertedSamples);
+int ConvertToAmp_Sensor1(int CurrentSample);
+int ConvertToAmp_Sensor2(int CurrentSample);
+
